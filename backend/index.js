e71368a5f1f54f693deee43becaed8b6d8ba898a
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-04-30 09:50:22 +0530

    add url

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1", rootRouter);
+app.use("https://paytm-app-2.onrender.com/api/v1", rootRouter);
 app.listen(process.env.PORT, () => {
   console.log("server start");
 });
