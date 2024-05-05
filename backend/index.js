03ce808eedb2ffbaf88f7738f3a70e4eb6e8f95b
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-05-05 20:40:17 +0530

    completee

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", rootRouter);
+app.get("/", (req, res) => {
+  return res.json({ msg: "server-start" });
+});
 app.listen(process.env.PORT, () => {
   console.log("server start");
 });
